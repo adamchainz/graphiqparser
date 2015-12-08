678678a7183c5d38880495a920f2706e0f5d782e
Cathy Deng <cathyd125@gmail.com>
2015-12-08 13:48:55 -0600

    adding training data

--- spotcheck/spotcheck.py
+++ spotcheck/spotcheck.py
@@ -4,21 +4,17 @@ import graphiqparser as gp
 infilename = 'raw/hillary_fec_employers.csv'
 outfilename = 'hillary_fec_labeled.csv'
 
-# expected format in input.csv: first column 'id', second column 'address'
 with open(infilename, 'rU') as f:
     reader = csvkit.DictReader(f)
 
     all_rows = []
     for row in reader:
-        # print "ROW", row
         raw_str = row['Employer']
         try:
             parsed = gp.tag(raw_str)
             row_dict = parsed[0]
         except:
             row_dict = {'error':'True'}
-
-        # print "PARSED", parsed
 
         row_dict['raw'] = raw_str
         all_rows.append(row_dict)
